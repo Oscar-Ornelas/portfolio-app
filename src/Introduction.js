f2999b2e15c7133f02abf9d3e15af2dc2d8951fe
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-17 10:48:48 -0600

    Removed scrolling on resume button

--- src/Introduction.js
+++ src/Introduction.js
@@ -12,7 +12,7 @@ function Introduction() {
         <p className="info">I'm here to become the best front-end web developer possible to provide as much value as possible to companies like yours.</p>
         <div class="intro-buttons">
           <a href="#projects" className="btn btn-intro">Check Out My Projects</a>
-          <a href="#projects" className="btn btn-intro">Download Resume</a>
+          <a href="#" className="btn btn-intro">Download Resume</a>
         </div>
       </div>
     </section>
