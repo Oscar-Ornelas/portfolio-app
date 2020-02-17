2b7c4a84758308633d6c6561e901d36bbf6fd433
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-16 23:30:50 -0600

    Updates

---
+++ precache-manifest.d290c7c5303b84c17747382751aab768.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1292845e3fc4c05d783e74b159461a6e",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "d97fa124deeeecf2f1d9",
+    "url": "/portfolio-app/static/css/main.33479277.chunk.css"
+  },
+  {
+    "revision": "ac4d5fee07fca0f1a4c2",
+    "url": "/portfolio-app/static/js/2.649a6e99.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio-app/static/js/2.649a6e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97fa124deeeecf2f1d9",
+    "url": "/portfolio-app/static/js/main.244fd407.chunk.js"
+  },
+  {
+    "revision": "4f9b97746fed4942fb04",
+    "url": "/portfolio-app/static/js/runtime-main.c721982f.js"
+  },
+  {
+    "revision": "0348d04c9d5725cd62ccffbef7fd69af",
+    "url": "/portfolio-app/static/media/calculator.0348d04c.PNG"
+  },
+  {
+    "revision": "a54ae1b66d7b9f49ba0990cae8b9c995",
+    "url": "/portfolio-app/static/media/post.a54ae1b6.jpg"
+  }
+]);
