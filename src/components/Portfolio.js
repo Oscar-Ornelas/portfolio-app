4c71b20d388d99674901d2e355c434d52dda371f
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-05-21 04:34:51 -0500

    Add new project descriptions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,19 +14,19 @@ function Portfolio() {
       <div className="portfolio">
         <Project
           img={film_box}
-          caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
-          repoURL="https://github.com/Oscar-Ornelas/calculator-app"
-          projectURL="https://oscar-ornelas.github.io/calculator-app/"
+          caption="An IMBd clone built using React which dynamically displays current film information including films now playing in theaters and most popular. This project utilizes The Movie Database API."
+          repoURL="https://github.com/Oscar-Ornelas/film-box"
+          projectURL="https://oscar-ornelas.github.io/film-box/"
         />
         <Project
           img={classical_junkie}
-          caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
-          repoURL="https://github.com/Oscar-Ornelas/calculator-app"
-          projectURL="https://oscar-ornelas.github.io/calculator-app/"
+          caption="A Spotify clone built in React for Spotify premium users which allows the user to search and play for classical music. This project utilizes the Spotify Web API and Web Playback SDK."
+          repoURL="https://github.com/Oscar-Ornelas/classical-junkie"
+          projectURL="https://oscar-ornelas.github.io/classical-junkie/"
         />
         <Project
           img={calculator}
-          caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
+          caption="A Calculator built in React to perform the functions required in most daily calculator usages from calculating percentages to performing several calculations on a single line."
           repoURL="https://github.com/Oscar-Ornelas/calculator-app"
           projectURL="https://oscar-ornelas.github.io/calculator-app/"
         />
--- src/components/PortfolioProject.js
+++ src/components/PortfolioProject.js
@@ -4,11 +4,13 @@ function PortfolioProject(props) {
   return (
     <div className="project">
       <img className="project-img" src={props.img}/>
-      <figcaption className="caption">{props.caption}</figcaption>
-      <span className="project-btns">
-        <a className="btn btn-project" href={props.repoURL} target="_blank" rel="noopener noreferrer">View Code</a>
-        <a className="btn btn-project" href={props.projectURL} target="_blank" rel="noopener noreferrer">View Project</a>
-      </span>
+      <div className="project-info">
+        <figcaption className="caption">{props.caption}</figcaption>
+        <span className="project-btns">
+          <a className="btn btn-project" href={props.repoURL} target="_blank" rel="noopener noreferrer">View Code</a>
+          <a className="btn btn-project" href={props.projectURL} target="_blank" rel="noopener noreferrer">View Project</a>
+        </span>
+      </div>
     </div>
   )
 }
