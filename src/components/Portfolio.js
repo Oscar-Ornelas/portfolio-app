e43cf476ba56e7128fcc4a4970c17ebd3dd8b35f
Oscar Ornelas <oscarornelas98123@gmail.com>
2020-05-21 03:40:17 -0500

    add new project images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import calculator from '../imgs/calculator.PNG';
+import film_box from '../imgs/film_box.PNG';
+import classical_junkie from '../imgs/classical_junkie.PNG';
 import Project from './PortfolioProject';
 
 function Portfolio() {
@@ -11,25 +13,13 @@ function Portfolio() {
       </div>
       <div className="portfolio">
         <Project
-          img={calculator}
+          img={film_box}
           caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
           repoURL="https://github.com/Oscar-Ornelas/calculator-app"
           projectURL="https://oscar-ornelas.github.io/calculator-app/"
         />
         <Project
-          img={calculator}
-          caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
-          repoURL="https://github.com/Oscar-Ornelas/calculator-app"
-          projectURL="https://oscar-ornelas.github.io/calculator-app/"
-        />
-        <Project
-          img={calculator}
-          caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
-          repoURL="https://github.com/Oscar-Ornelas/calculator-app"
-          projectURL="https://oscar-ornelas.github.io/calculator-app/"
-        />
-        <Project
-          img={calculator}
+          img={classical_junkie}
           caption="Calculator built entirely in React with mobile-responsive CSS and semantic HTML"
           repoURL="https://github.com/Oscar-Ornelas/calculator-app"
           projectURL="https://oscar-ornelas.github.io/calculator-app/"
